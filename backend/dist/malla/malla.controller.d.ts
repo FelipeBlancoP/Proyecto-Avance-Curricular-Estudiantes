36e412baa12a18381726860eb17fa5eb1188d3d3
Francisco Plaza <francisco.plaza@alumnos.ucn.cl>
2025-10-09 02:53:23 -0300

    chore: Dejar de rastrear node_modules y dist en main

--- backend/dist/malla/malla.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { MallaService } from './malla.service';
-export declare class MallaController {
-    private readonly mallaService;
-    constructor(mallaService: MallaService);
-    obtenerMalla(codigoCarrera: string): Promise<any>;
-}
--- backend/dist/malla/malla.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-export declare class MallaService {
-    private readonly configService;
-    constructor(configService: ConfigService);
-    obtenerMalla(codigoCarrera: string): Promise<any>;
-}
